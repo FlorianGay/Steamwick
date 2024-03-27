17d7a0b549dd6ced66465b7c8c6b2a00d442e39c
Florian <floriangay750@gmail.com>
2024-03-27 14:09:45 +0100

    Création pages placeToVisit finition page categoryPlace et placeDetails

--- src/pages/placeToVisit/categoryPlace.jsx
+++ src/pages/placeToVisit/categoryPlace.jsx
@@ -1,30 +1,39 @@
 import { useLocation } from 'react-router-dom'
 import placeList from '../../data/places.json'
 import { Link } from 'react-router-dom'
+import { arrowLeft } from '../../assets/icon'
 
 import './categoryPlace.scss'
 
 function CategoryPlace() {
-  const location = useLocation();
+  const location = useLocation()
   const locationCategory = location.pathname.split('/').at(-1)
   const placeFiltered = placeList.filter((list) =>
     list.category.includes(locationCategory)
   )
   return (
     <main>
-      <h1 className="category-title">{locationCategory}</h1>
+      <div className="category-up">
+        <Link to={'/place-to-visit'}>
+          <button>{arrowLeft}</button>
+        </Link>
+        <h1 className="category-title">{locationCategory}</h1>
+      </div>
+
       <div className="category-card">
         {placeFiltered.map((list, index) => (
-            <Link to={`/place-to-visit/${locationCategory}/${list.id}`} key={index}>
+          <Link
+            to={`/place-to-visit/${locationCategory}/${list.id}`}
+            key={index}
+          >
             <figure>
-            <img src={list.image} alt={list.title} />
-            <figcaption>
-              <h3>{list.title}</h3>
-              <p>{list.description}</p>
-            </figcaption>
-          </figure>
-            </Link>
-          
+              <img src={list.image} alt={list.title} />
+              <figcaption>
+                <h3>{list.title}</h3>
+                <p>{list.description}</p>
+              </figcaption>
+            </figure>
+          </Link>
         ))}
       </div>
     </main>
--- src/pages/placeToVisit/placeDetails.jsx
+++ src/pages/placeToVisit/placeDetails.jsx
@@ -1,23 +1,33 @@
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import placeList from '../../data/places.json'
+import './placeDetails.scss'
+import { arrowLeft } from '../../assets/icon'
 
 function PlaceDetails() {
   const location = useLocation()
   const locationId = location.pathname.split('/').at(-1)
-  const placeFiltered = placeList.find((list) => list.id === parseInt(locationId))
+  const placeFiltered = placeList.find(
+    (list) => list.id === parseInt(locationId)
+  )
 
   return (
     <main className="place-details">
-      <h2>{placeFiltered.title}</h2>
+      <div className="place-up">
+        <Link to={`/place-to-visit/${placeFiltered.category}`}>
+          <button>{arrowLeft}</button>
+        </Link>
+        <h2>{placeFiltered.title}</h2>
+      </div>
+
       <article>
         <img src={placeFiltered.image} alt={placeFiltered.title} />
-        <div className="place-card">
+        <div>
           <section className="place-information">
             <p>
               <strong>Prix d'entrée : {placeFiltered.price}</strong>
             </p>
             <p>
-              <strong>Horaires : : {placeFiltered.open_time}</strong>
+              <strong>Horaires : {placeFiltered.open_time}</strong>
             </p>
             <p>
               <strong>Temps de visite : {placeFiltered.time}</strong>
