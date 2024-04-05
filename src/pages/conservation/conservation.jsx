aa608d3265f1545883be7071cb7e446b695508df
Florian <floriangay750@gmail.com>
2024-04-05 15:06:50 +0200

    Responsive page conservation Création + responsive page 404

--- src/pages/conservation/conservation.jsx
+++ src/pages/conservation/conservation.jsx
@@ -6,48 +6,44 @@ function Conservation() {
   return (
     <main className="conservation">
       <h1>Conservation</h1>
-      <section className='normal'>
+      <section >
         <h2>Manoir des Horloges</h2>
-        <figure>
+        <figure >
           <img src={manorImg} alt="Photo de la réstauration d'un manoir" />
           <figcaption>
-            
-              <h3>Initiative du projet</h3>
-              <p>
-                Le Manoir des Horloges est un projet de restauration ambitieux
-                visant à préserver et restaurer l'un des bâtiments historiques
-                les plus emblématiques de Steamwick. Ce manoir, autrefois la
-                demeure d'une famille influente de l'époque victorienne, a été
-                le témoin de nombreuses soirées élégantes et de réceptions
-                prestigieuses. Malheureusement, au fil des ans, le manoir a
-                souffert de l'usure du temps, avec des parties de sa structure
-                nécessitant une rénovation urgente. L'initiative de restauration
-                vise à redonner au Manoir des Horloges sa splendeur d'antan,
-                tout en préservant son histoire et son importance dans le tissu
-                social de Steamwick.
-              </p>
-            
-            
-              <h3>Fonction du lieu</h3>
-              <p>
-                Une fois restauré, le Manoir des Horloges retrouvera sa fonction
-                d'origine en tant que lieu de rassemblement élégant pour les
-                événements culturels et sociaux de la ville. Il accueillera à
-                nouveau des réceptions, des galas de charité, des bals masqués
-                et d'autres événements spéciaux. Le manoir sera également ouvert
-                au public pour des visites guidées, offrant aux habitants et aux
-                visiteurs un aperçu de l'histoire fascinante de Steamwick à
-                travers les salons magnifiquement restaurés, les salles de bal
-                impressionnantes et les jardins pittoresques. Cet espace
-                restauré sera non seulement un joyau architectural, mais aussi
-                un lieu vibrant de la vie culturelle de la ville.
-              </p>
-            
+            <h3>Initiative du projet</h3>
+            <p>
+              Le Manoir des Horloges est un projet de restauration ambitieux
+              visant à préserver et restaurer l'un des bâtiments historiques les
+              plus emblématiques de Steamwick. Ce manoir, autrefois la demeure
+              d'une famille influente de l'époque victorienne, a été le témoin
+              de nombreuses soirées élégantes et de réceptions prestigieuses.
+              Malheureusement, au fil des ans, le manoir a souffert de l'usure
+              du temps, avec des parties de sa structure nécessitant une
+              rénovation urgente. L'initiative de restauration vise à redonner
+              au Manoir des Horloges sa splendeur d'antan, tout en préservant
+              son histoire et son importance dans le tissu social de Steamwick.
+            </p>
+
+            <h3>Fonction du lieu</h3>
+            <p>
+              Une fois restauré, le Manoir des Horloges retrouvera sa fonction
+              d'origine en tant que lieu de rassemblement élégant pour les
+              événements culturels et sociaux de la ville. Il accueillera à
+              nouveau des réceptions, des galas de charité, des bals masqués et
+              d'autres événements spéciaux. Le manoir sera également ouvert au
+              public pour des visites guidées, offrant aux habitants et aux
+              visiteurs un aperçu de l'histoire fascinante de Steamwick à
+              travers les salons magnifiquement restaurés, les salles de bal
+              impressionnantes et les jardins pittoresques. Cet espace restauré
+              sera non seulement un joyau architectural, mais aussi un lieu
+              vibrant de la vie culturelle de la ville.
+            </p>
           </figcaption>
         </figure>
-        </section>
+      </section>
 
-        <section className='reverse'>
+      <section >
         <h2>Ancienne usine à vapeur</h2>
         <figure>
           <img
--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -1,8 +1,25 @@
+import { Link } from 'react-router-dom'
+import errorImg from '../../assets/error.webp'
+import './error.scss'
 
 function Error() {
-    return(
-        <></>
-    )
+  return (
+    <main>
+      <section className="error">
+        <figure>
+          <img src={errorImg} alt="logo error 404 de style steampunk" />
+          <figcaption>
+            <h1>Page non trouvée</h1>
+            <p>
+              La page que vous recherchez n'existe pas ou n'est pas disponible.
+              <br />
+            </p>
+            <Link to={'/'}>Retour sur la page d'acceuil</Link>
+          </figcaption>
+        </figure>
+      </section>
+    </main>
+  )
 }
 
 export default Error
